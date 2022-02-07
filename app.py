e5b408c07c8d2486d3b50f2ffafcf4c5dfc0d3d2
ansh-dagha <ansh.dagha@somaiya.edu>
2022-02-07 15:24:47 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for, request, session, redirect
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.route('/', methods=['GET', 'POST'])
+def basic():
+    return render_template('index.html')
+
+#---------------------------------------- Main ----------------------------------------#
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(debug=True)
